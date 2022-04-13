57072ffc8587c791a0e9fa42dbe5056482d72126
seraslan <seraslan@metu.edu.tr>
2022-04-13 14:22:00 +0300

    Main creates a customer object

--- src/com/project/Main.java
+++ src/com/project/Main.java
@@ -3,6 +3,6 @@ package com.project;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Customer c = new Customer(12,"test customer",'m');
     }
 }
